df99bee53a88c46a969327bc084c3f75f991d162
Rowan Merewood <merewood@google.com>
2017-05-09 10:25:26 +0100

    Update to new JS and a little more mobile friendly

--- examples/example-captcha.php
+++ examples/example-captcha.php
@@ -45,6 +45,7 @@ $lang = 'en';
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="viewport" content="width=device-width,height=device-height,minimum-scale=1">
         <title>reCAPTCHA Example</title>
         <link rel="shortcut icon" href="//www.gstatic.com/recaptcha/admin/favicon.ico" type="image/x-icon"/>
         <style type="text/css">
